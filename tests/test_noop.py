9b572bf76dac2f9ae54c3d3ad4d01a0fc74d591a
kristopher <kristopher@acm.org>
2024-08-19 04:47:21 -0500

    add a noop test :deciduous_tree:

---
+++ tests/test_noop.py
@@ -0,0 +1,10 @@
+import unittest
+
+class TestNoOp(unittest.TestCase):
+    def test_noop(self):
+        """A basic no-op test."""
+        pass  # No operation, this test will always pass
+
+if __name__ == '__main__':
+    unittest.main()
+
