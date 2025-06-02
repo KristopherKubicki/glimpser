54c67f1e2a7abba25490346679784ed4aba93201
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-06-02 05:58:33 -0500

    feat: expand danger mode page

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -5,6 +5,7 @@ import { initNav } from './nav.js';
 import { initFormValidation } from './form.js';
 import { initFooterFade } from './footer.js';
 import { initOffline } from './offline.js';
+import { initDangerToggle } from './danger.js';
 
 initTemplates();
 initVideoControls();
@@ -13,3 +14,4 @@ initNav();
 initFormValidation();
 initFooterFade();
 initOffline();
+initDangerToggle();
