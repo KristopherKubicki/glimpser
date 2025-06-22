4e2f31df5c086618ff8b251d1d31fa5e9805ce3b
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-06-22 07:36:42 -0500

    fix: handle truncated images

--- app/utils/scheduling.py
+++ app/utils/scheduling.py
@@ -1339,9 +1339,11 @@ def cache_logs():
     try:
         with open(log_file_path, "r") as file:
             file.seek(0, os.SEEK_END)  # Start at end of file
-            while not stop_event.is_set():
+            while True:
                 new_log = file.readline()
                 if not new_log:
+                    if stop_event.is_set():
+                        break
                     # Avoid busy looping when no new log lines are written.
                     time.sleep(1)
                     # The file may have been truncated. Seek to end and retry.
