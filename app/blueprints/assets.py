a07c5ce67e68cf46bc626d68693216b3ab4e7454
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-06-22 22:41:47 -0500

    fix(login): hide recovery note for remote users

--- app/blueprints/assets.py
+++ app/blueprints/assets.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
 
-from pathlib import Path
 import logging
 import os
 import re
 import time
+from pathlib import Path
 
 from flask import Blueprint, Response
 
