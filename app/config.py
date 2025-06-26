5116a52691c0ba011c0e401cdb415e8ba26a6416
Kristopher Kubicki <478212+KristopherKubicki@users.noreply.github.com>
2025-06-26 08:55:39 -0500

    feat(settings): add notification toggles

--- app/config.py
+++ app/config.py
@@ -520,6 +520,10 @@ TWILIO_FROM_NUMBER = get_setting("TWILIO_FROM_NUMBER", "")
 VAPID_PUBLIC_KEY = get_setting("VAPID_PUBLIC_KEY", "")
 VAPID_PRIVATE_KEY = get_setting("VAPID_PRIVATE_KEY", "")
 
+# Notification toggles
+NOTIFY_ON_MOTION = get_setting("NOTIFY_ON_MOTION", "True") == "True"
+NOTIFY_ON_CAPTION = get_setting("NOTIFY_ON_CAPTION", "True") == "True"
+
 # Common Alerting Protocol settings
 CAP_ENABLED = get_setting("CAP_ENABLED", "False")
 CAP_ENDPOINT = get_setting("CAP_ENDPOINT", "")
